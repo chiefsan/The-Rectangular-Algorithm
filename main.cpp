1d277b346f40579580b36b3bc6ad431925c0b877
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-04-27 19:05:31 +0530

    Update main.cpp
    Add comments, rename some variables and format

--- main.cpp
+++ main.cpp
@@ -1,36 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 using namespace std::chrono;
 
-int ** randGraph(int n) {
-    int **a=new int*[n];
-    for(int i=0;i<n;i++) {
-        a[i]=new int[n];
-    }
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<n;j++) {
-            if(i==j) {
-                a[i][j]=0;
-            }
-            else {
-                a[i][j]=rand()%1000+1;
-                if (a[i][j]%2)
-                a[i][j] = 0;
-            }
-          }
+/*
+  Generate a random graph (adjacency matrix representation)
+  
+  Parameters
+  n : int, number of vertices in the random graph
+
+  Output
+  Adjacency matrix,  int**
+*/
+int **randGraph(int n)
+{
+  int **a = new int *[n];
+  for (int i = 0; i < n; i++)
+  {
+    a[i] = new int[n];
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (i == j)
+      {
+        a[i][j] = 0;
       }
-    return (a);
+      else
+      {
+        a[i][j] = rand() % 1000 + 1;
+        if (a[i][j] % 2)
+          a[i][j] = 0;
+      }
+    }
+  }
+  return (a);
 }
 
-int main () {
-  int n = 300;
-  int **a=randGraph(n);
+int main(int argc, char **argv)
+{
+
+  int num_vertices = atoi(argv[1]);
+
+  int **adj = randGraph(num_vertices);
   ofstream myfile("input.txt");
-  for (int i=0; i<n; i++)
-    for (int j=0; j<n; j++) {
-      myfile<< a[i][j] << ' ';
-  }
+  for (int i = 0; i < num_vertices; i++)
+    for (int j = 0; j < num_vertices; j++)
+    {
+      myfile << adj[i][j] << ' ';
+    }
+
   myfile.close();
   system("g++ -o fw floyd-warshall.cpp");
   system("g++ -o ra rectangular.cpp");
@@ -46,6 +66,4 @@ int main () {
   stop = high_resolution_clock::now();
   duration = duration_cast<microseconds>(stop - start);
   cout << "Time taken by function ra: " << duration.count() << " microseconds" << endl;
-
-
 }
