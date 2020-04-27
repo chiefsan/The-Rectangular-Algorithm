fed2078e0b10fcdeb4414c5d56475832ca7c98af
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-04-27 19:05:38 +0530

    Update floyd-warshall.cpp
    Add comments, rename some variables and format

--- floyd-warshall.cpp
+++ floyd-warshall.cpp
@@ -1,17 +1,29 @@
 #include <bits/stdc++.h>
 
-void print(std::vector<std::vector<double>> dist, std::vector<std::vector<int>> next) {
+/*
+Print the solution
+
+Parameters
+dist - distance matrix, vector<std::vector<double>>
+next - next matrix, vector<std::vector<int>>
+*/
+void print(std::vector<std::vector<double>> dist, std::vector<std::vector<int>> next)
+{
   const auto size = next.size();
-  for (auto i = 0; i < size; ++i) {
-    for (auto j = 0; j < size; ++j) {
-      if (i != j) {
+  for (auto i = 0; i < size; ++i)
+  {
+    for (auto j = 0; j < size; ++j)
+    {
+      if (i != j)
+      {
         auto u = i + 1;
         auto v = j + 1;
         std::cout << "(" << u << " -> " << v << ", " << dist[i][j]
-          << ", ";
+                  << ", ";
         std::stringstream path;
         path << u;
-        do {
+        do
+        {
           u = next[u - 1][v - 1];
           path << " -> " << u;
         } while (u != v);
@@ -21,55 +33,72 @@ void print(std::vector<std::vector<double>> dist, std::vector<std::vector<int>> 
   }
 }
 
-void solve(std::vector<std::vector<double>> dist, const int num_vertices) {
-  auto start = std::chrono::high_resolution_clock::now();
+/*
+Compute the solution
+
+Parameters
+dist - distance matrix, vector<std::vector<double>>
+num_vertices - number of vertices, int
+*/
+void solve(std::vector<std::vector<double>> dist, const int num_vertices)
+{
+  // Uncomment lines 40, 60-64 to dump running time in a file (fw_output.txt)
+  // auto start = std::chrono::high_resolution_clock::now();
   std::vector<std::vector<int>> next(num_vertices);
-  for (auto i = 0; i < num_vertices; i++) {
-    for (auto j = 0; j < num_vertices; j++) {
+  for (auto i = 0; i < num_vertices; i++)
+  {
+    for (auto j = 0; j < num_vertices; j++)
+    {
       next[i].push_back(0);
-    }
-    for (auto j = 0; j < num_vertices; j++) {
-      if (i != j) {
+      if (i != j)
+      {
         next[i][j] = j + 1;
       }
     }
   }
-  for (auto k = 0; k < num_vertices; k++) {
-    for (auto i = 0; i < num_vertices; i++) {
-      for (auto j = 0; j < num_vertices; j++) {
-        if (dist[i][j] > dist[i][k] + dist[k][j]) {
+  for (auto k = 0; k < num_vertices; k++)
+  {
+    for (auto i = 0; i < num_vertices; i++)
+    {
+      for (auto j = 0; j < num_vertices; j++)
+      {
+        if (dist[i][j] > dist[i][k] + dist[k][j])
+        {
           dist[i][j] = dist[i][k] + dist[k][j];
           next[i][j] = next[i][k];
         }
       }
     }
   }
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-  std::ofstream myfile("fw_output.txt");
-  myfile << duration.count();
-  myfile.close();
-  // print(dist, next);
+  // auto stop = std::chrono::high_resolution_clock::now();
+  // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  // std::ofstream myfile("fw_output.txt");
+  // myfile << duration.count();
+  // myfile.close();
 }
 
-int main (int argc, char **argv) {
-  int n = atoi(argv[1]);
-  std::vector<std::vector<double>> w(n);
-  for (int i=0; i<n; i++) {
-    w[i].resize(n);
+int main(int argc, char **argv)
+{
+  auto num_vertices = atoi(argv[1]);
+
+  std::vector<std::vector<double>> w(num_vertices);
+  for (int i = 0; i < num_vertices; i++)
+  {
+    w[i].resize(num_vertices);
   }
-  int temp;
+
   std::ifstream myfile("input.txt", std::ios::in);
-  for (int i=0; i<n; i++)
-    for (int j=0; j<n; j++) {
-      myfile>>temp;
-      w[i][j] = temp;
-      if (w[i][j]==0)
+  for (int i = 0; i < num_vertices; i++)
+    for (int j = 0; j < num_vertices; j++)
+    {
+      myfile >> w[i][j];
+      if (w[i][j] == 0)
         w[i][j] = INT_MAX;
     }
 
-  int num_vertices = n;
+  solve(w, num_vertices);
 
-  solve (w, num_vertices);
+  w.clear();
+
   return 0;
 }
